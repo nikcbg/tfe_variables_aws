508276e4f881f539de5a62f116413c5ba77dcd0c
nikolay <nikcbg@yahoo.com>
2019-06-11 11:37:58 +0300

    update main.tf to version0.12

--- main.tf
+++ main.tf
@@ -1,22 +1,21 @@
 variable "access_key" {}
 variable "secret_key" {}
-variable "key_name" {}
 variable "region" {}
 variable "ami" {}
 variable "instance_type" {}
 
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region = "${var.region}"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
 }
 
 resource "aws_instance" "example" {
-  ami = "${var.ami}"
-  instance_type = "${var.instance_type}"
-  key_name = "${var.key_name}"
-}        
+  ami           = var.ami
+  instance_type = var.instance_type
+}
 
 output "public_dns" {
-  value = "${aws_instance.example.public_dns}"
+  value = aws_instance.example.public_dns
 }
+
